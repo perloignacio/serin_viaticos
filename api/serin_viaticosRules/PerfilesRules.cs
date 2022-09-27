42b5221f4a969a8e7d486f90c7f606f3f28e015c
visualcnp <visualcnp@yahoo.com.ar>
2022-09-27 12:29:22 -0300

    PerfilesRules

---
+++ api/serin_viaticosRules/PerfilesRules.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serin_viaticosRules
+{
+    class PerfilesRules
+    {
+        public void Agregar() { }
+
+    }
+}
