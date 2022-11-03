c641c4b568bb02b85c453da581fd8642ffe188ce
visualcnp <visualcnp@yahoo.com.ar>
2022-11-03 17:31:01 -0300

    2022-11-03 CAMBIOS

---
+++ api/WebApi/Controllers/SolicitudesDetalleController.cs
@@ -0,0 +1,99 @@
+﻿using serin_viaticosRules;
+using serin_viaticosRules.Entities;
+using serin_viaticosRules.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("SolicitudesDetalle")]
+    public class SolicitudesDetalleController : ApiController
+    {
+        [Route("GetTodos")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult GetTodos()
+        {
+            try
+            {
+                return Ok(SolicitudesDetalleMapper.Instance().GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        
+        [Route("Uno/{IdSolicitudDetalle}")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult Uno(int IdSolicitudDetalle)
+        {
+            try
+            {
+                return Ok(SolicitudesDetalleMapper.Instance().GetOne(IdSolicitudDetalle));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        
+
+        [Route("AgregarEditar/{IdSolicitudDetalle?}")]
+        [HttpPost]
+        [AllowAnonymous]
+        public IHttpActionResult AgregarEditar([FromBody] SolicitudesDetalle pf, int? IdSolicitudDetalle = null)
+        {
+            try
+            {
+                SolicitudesDetalleRules pfRules = new SolicitudesDetalleRules();
+                if (IdSolicitudDetalle.HasValue)
+                {
+                    pfRules.Modificar(IdSolicitudDetalle.Value, pf.IdSolicitud, pf.IdSolicitudCategoria, pf.IdReservaAereo, pf.IdResevaHotel, pf.IdItinerario, pf.IdReservaAlquilerAuto, pf.Observaciones);
+                }
+                else
+                {
+                    pfRules.Agregar(pf.IdSolicitud, pf.IdSolicitudCategoria, pf.IdReservaAereo, pf.IdResevaHotel, pf.IdItinerario, pf.IdReservaAlquilerAuto, pf.Observaciones);
+                }
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        
+
+
+        [Route("Borrar/{IdSolicitudDetalle}")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public IHttpActionResult Borrar(int IdSolicitudDetalle)
+        {
+            try
+            {
+                SolicitudesDetalleRules pf = new SolicitudesDetalleRules();
+                pf.Eliminar(IdSolicitudDetalle);
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    
+    }
+}
---
+++ api/WebApi/Controllers/SolicitudesUsuariosController.cs
@@ -0,0 +1,99 @@
+﻿using serin_viaticosRules;
+using serin_viaticosRules.Entities;
+using serin_viaticosRules.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("SolicitudesUsuarios")]
+    public class SolicitudesUsuariosController : ApiController
+    {
+        [Route("GetTodos")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult GetTodos()
+        {
+            try
+            {
+                return Ok(SolicitudesUsuariosMapper.Instance().GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        
+        [Route("Uno/{IdSolicitudUsuario}")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult Uno(int IdSolicitudUsuario)
+        {
+            try
+            {
+                return Ok(SolicitudesUsuariosMapper.Instance().GetOne(IdSolicitudUsuario));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        /*
+        [Route("AgregarEditar/{IdSolicitudUsuario?}")]
+        [HttpPost]
+        [AllowAnonymous]
+        public IHttpActionResult AgregarEditar([FromBody] SolicitudesUsuarios pf, int? IdSolicitud = null)
+        {
+            try
+            {
+                SolicitudesUsuariosRules pfRules = new SolicitudesUsuariosRules();
+                if (IdSolicitud.HasValue)
+                {
+                    pfRules.Modificar(IdSolicitud.Value, pf.IdSolicitudUsuario, pf.IdUsuario, pf.MontoAnticipo);
+                }
+                else
+                {
+                    pfRules.Agregar(pf.IdSolicitudUsuario, pf.IdUsuario, pf.MontoAnticipo);
+                }
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        
+
+
+        [Route("Borrar/{IdSolicitudUsuario}")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public IHttpActionResult Borrar(int IdSolicitudUsuario)
+        {
+            try
+            {
+                SolicitudesUsuariosRules pf = new SolicitudesUsuariosRules();
+                pf.Eliminar(IdSolicitudUsuario);
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+       
+        */
+    }
+}
